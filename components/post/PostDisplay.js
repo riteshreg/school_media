0f19cc547bcfc240294f4453ecf804f3aa40ba5a
Ritesh Regmi <riteshregmi85@gmail.com>
2023-02-06 17:49:24 +0545

    time ago

--- components/post/PostDisplay.js
+++ components/post/PostDisplay.js
@@ -5,60 +5,78 @@ import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Avatar from "../Avatar";
 import Card from "../Card";
-import { PhotoProvider, PhotoView } from 'react-photo-view';
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import Linkify from "react-linkify";
+import ReactTimeAgo from 'react-time-ago'
 
+export default function PostDispaly({
+  id,
+  content,
+  images,
+  created_at,
+  profile,
+  loginUser,
+}) {
+  const supabase = useSupabaseClient();
 
-export default function PostDispaly({id, content, images, created_at, profile,loginUser }) {
+  console.log(created_at)
 
-  const supabase = useSupabaseClient()
+  const [likes, setLikes] = useState([]);
+  const [disabledLike, setDisabledLike] = useState(false);
 
-  const[likes, setLikes] = useState([])
-  const [disabledLike, setDisabledLike] = useState(false)
-  
-  const router = useRouter()
-  
+  const router = useRouter();
 
-
-  const handleLike  = () =>{
-      setDisabledLike(true)
-    if(!IAlreadyLiked){
-      supabase.from("likes").insert({
-          user_id:loginUser?.id,
-          post_id:id
-      }).then((resposne)=>{
-          if(resposne.status == 201){
-            GetAllLikes()
+  const handleLike = () => {
+    setDisabledLike(true);
+    if (!IAlreadyLiked) {
+      supabase
+        .from("likes")
+        .insert({
+          user_id: loginUser?.id,
+          post_id: id,
+        })
+        .then((resposne) => {
+          if (resposne.status == 201) {
+            GetAllLikes();
           }
-      })
+        });
     }
+  };
+
+  useEffect(() => {
+    GetAllLikes();
+  }, []);
+
+  function GetAllLikes() {
+    supabase
+      .from("likes")
+      .select()
+      .eq("post_id", id)
+      .then((res) => setLikes(res.data));
   }
 
-  useEffect(()=>{
-    GetAllLikes()
-  },[])
-
-  function GetAllLikes(){
-    supabase.from('likes').select().eq("post_id", id).then((res)=>setLikes(res.data))
-  }
-
- const IAlreadyLiked =  !!likes?.find(like=>like.user_id == loginUser.id)
+  const IAlreadyLiked = !!likes?.find((like) => like.user_id == loginUser.id);
 
   return (
     <div className="py-1">
       <Card className="py-1">
         <div className="px-4 flex items-center gap-3">
           <div className="p-1">
-          <Avatar />
+            <Avatar />
           </div>
-          <label className="mb-5 ">
+          <label className="mb-5 flex flex-col ">
             <span className="mr-2 font-semibold text-gray-700">
-              {"Janata Mavi Gauradaha"}
+              {"Janata Mavi Official"}
             </span>
-            Share a Post
+            <ReactTimeAgo className="font-sans text-sm" date={created_at} locale="en-US"/>
           </label>
         </div>
         <div className="mx-2 my-1 p-1 text-md">
-          <p className="text-left">{content}</p>
+          <div className="all-initial">
+          <Linkify>
+          <p>{content}</p>
+          </Linkify>
+          </div>
         </div>
         {images?.length > 0 && (
           <div
@@ -66,28 +84,32 @@ export default function PostDispaly({id, content, images, created_at, profile,lo
               images.length > 2 && "block md:grid md:grid-cols-2"
             }`}
           >
-               { <PhotoProvider>
-
-            {images.map((image) => (
-               <PhotoView key={image} src={image}>
-              <Image
-                alt="photo"
-                height={700}
-                className="p-2 cursor-pointer rounded-sm overflow-hidden"
-                width={650}
-                src={image}
-              />
-              </PhotoView>
-            ))}
-                </PhotoProvider>
-          }
-
+            {
+              <PhotoProvider>
+                {images.map((image) => (
+                  <PhotoView key={image} src={image}>
+                    <Image
+                      alt="photo"
+                      height={700}
+                      className="p-2 cursor-pointer rounded-sm overflow-hidden"
+                      width={650}
+                      src={image}
+                    />
+                  </PhotoView>
+                ))}
+              </PhotoProvider>
+            }
           </div>
         )}
         <div className="pl-2 py-3">
-          <div  className="ml-4 flex gap-2 items-center">
-           <HeartIcon  onClick={handleLike}  className={`${disabledLike && 'pointer-events-none'} h-8  ${IAlreadyLiked && "fill-red-600"}`}/> 
-           {likes?.length}
+          <div className="ml-4 flex gap-2 items-center">
+            <HeartIcon
+              onClick={handleLike}
+              className={`${disabledLike && "pointer-events-none"} h-8  ${
+                IAlreadyLiked && "fill-red-600"
+              }`}
+            />
+            {likes?.length}
           </div>
         </div>
       </Card>
--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,13 @@ import { UserContext, UserContextProvider } from "@/UserContext";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState } from "react";
+import TimeAgo from "javascript-time-ago";
+import en from 'javascript-time-ago/locale/en.json'
+
 
 export default function App({ Component, pageProps }) {
-
-
+  
+  TimeAgo.addDefaultLocale(en);
 
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
 
