7ed9e7c57433be0ac8c1cfe04837b8501be0304f
Ritesh Regmi <riteshregmi85@gmail.com>
2023-02-11 15:09:45 +0545

    contact

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,13 @@
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import ReactLinkify from "react-linkify";
 import { MutatingDots } from "react-loader-spinner";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-
-const notify = () => toast.error('something went wrong', {
+const notify = () =>
+  toast.error("something went wrong", {
     position: "bottom-center",
     autoClose: 5000,
     hideProgressBar: false,
@@ -15,8 +16,7 @@ const notify = () => toast.error('something went wrong', {
     draggable: true,
     progress: undefined,
     theme: "light",
-    });
-
+  });
 
 export default function LoginPage() {
   const supabase = useSupabaseClient();
@@ -31,8 +31,8 @@ export default function LoginPage() {
   const [error, setError] = useState(false);
 
   const handleChange = (event) => {
-    setError(false)
-    setProgress(false)
+    setError(false);
+    setProgress(false);
     setLoginUser((prev) => ({
       ...prev,
       [event.target.name]: event.target.value,
@@ -40,32 +40,34 @@ export default function LoginPage() {
   };
 
   const handleLogin = async () => {
-    if(loginUser.email && loginUser.password){
-    setProgress(true)
-    const { data, error } = await supabase.auth.signInWithPassword(loginUser);
-    if(error){
-        setError(true)
-        setProgress(false)
-        notify()
+    if (loginUser.email && loginUser.password) {
+      setProgress(true);
+      const { data, error } = await supabase.auth.signInWithPassword(loginUser);
+      if (error) {
+        setError(true);
+        setProgress(false);
+        notify();
+      }
+      if (data.session?.access_token) {
+        setProgress(false);
+        router.push("/");
+      }
     }
-    if (data.session?.access_token) {
-        setProgress(false)
-      router.push("/");
-    }
-}
   };
 
-  const style = `px-1 w-56 py-2 border ${!error &&"border-gray-300" || error && 'border-red-500 border-2'} rounded-md outline-green-300`;
+  const style = `px-1 w-56 py-2 border ${
+    (!error && "border-gray-300") || (error && "border-red-500 border-2")
+  } rounded-md outline-green-300`;
 
   return (
     <div className="h-screen w-screen flex flex-col items-center justify-center">
       <div className="flex flex-col space-y-4">
         <input
-        onKeyDown={(e)=>{
-          if(e.key==="Enter"){
-            handleLogin()
-          }
-        }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleLogin();
+            }
+          }}
           type="email"
           placeholder="email"
           className={style}
@@ -73,11 +75,11 @@ export default function LoginPage() {
           onChange={handleChange}
         />
         <input
-        onKeyDown={(e)=>{
-          if(e.key==="Enter"){
-            handleLogin()
-          }
-        }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleLogin();
+            }
+          }}
           type="password"
           placeholder="password"
           className={style}
@@ -110,7 +112,7 @@ export default function LoginPage() {
           visible={true}
         />
       )}
-        <ToastContainer
+      <ToastContainer
         position="bottom-center"
         autoClose={5000}
         hideProgressBar={false}
@@ -120,7 +122,17 @@ export default function LoginPage() {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme="light" />
+        theme="light"
+      />
+
+      <div className="mt-5">
+        <ReactLinkify>
+          <h1 className="text-lg font-mono">
+        No credentials, Contact on
+           <span className="text-blue-600 ml-2">riteshregmi@gmail.com</span>{" "}
+          </h1>
+        </ReactLinkify>
+      </div>
     </div>
   );
 }
