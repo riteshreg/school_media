2c12f75d35c8748c6485f6147a85ca837a849b8f
Ritesh Regmi <71575903+riteshreg@users.noreply.github.com>
2023-03-03 14:25:01 +0545

    profile update
    ds

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-const  link="https://ypticbcztdwpynckjwag.supabase.co/storage/v1/object/public/images/305480223_576509657594875_840635747841878545_n%20(2).jpg"
+const  link="https://ypticbcztdwpynckjwag.supabase.co/storage/v1/object/public/images/1675576446803305480223_576509657594875_840635747841878545_n.jpg"
 export default function Avatar(){
     return(
             <Image 
